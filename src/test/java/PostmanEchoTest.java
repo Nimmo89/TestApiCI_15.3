ec4da07cee2f25e8db340376061e854496bd7b3c
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-09-27 12:58:50 +0700

    Repair Test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -10,7 +10,7 @@ public class PostmanEchoTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some noTest") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("some test") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
